246af9f449f55ebaaf5e1511ac25a07642776ac8
Hamid Reza Khoramrokh <hr.kh1380@gmail.com>
2025-01-19 21:19:55 -0500

    First write to sqlite from C

--- main.c
+++ main.c
@@ -4,11 +4,35 @@
 
 static int callback(void *notUsed, int argc, char **argv, char **azColName)
 {
-    
+    int i;
+    for (i=0; i<argc; i++){
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
 }
 
-int main()
+int main(int argc, char **argv)
 {
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    if ( argc != 3 ){
+        printf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
+        return(1);
+    }
+    rc = sqlite3_open(argv[1], &db);
+    if ( rc ){
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return(1);
+    }
+    rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
+    if ( rc != SQLITE_OK ){
+       fprintf(stderr, "SQL error: %s\n", zErrMsg);
+       sqlite3_free(zErrMsg);
+       return(1); 
+    }
+    sqlite3_close(db);
+
     printf("hello world\n");
     return 0;
 }
