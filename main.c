f1fec42813d5585fc9b30ad7522b06885ba8438e
Hamid Reza Khoramrokh <hr.kh1380@gmail.com>
2025-01-19 19:56:20 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sqlite3.h>
+
+
+static int callback(void *notUsed, int argc, char **argv, char **azColName)
+{
+    
+}
+
+int main()
+{
+    printf("hello world\n");
+    return 0;
+}
+
+
